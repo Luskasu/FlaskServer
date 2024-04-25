5797ce5220010383b0f43b660be151c0bdd7e500
Luskasu <lucas.araujo.barboza@gmail.com>
2024-04-25 19:36:34 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/upload', methods=['POST'])
+
+def upload():
+    file = request.files['file']
+    
+    file.save('files\ ' + file.filename) 
+    return 'upado com sucesso'
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port = 5000)
